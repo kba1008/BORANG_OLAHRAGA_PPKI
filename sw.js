457227d06893163bd7aef9a753885258b006724d
kba1008 <sekolah-4896@moe-dl.edu.my>
2026-08-05 07:21:51 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  *  - Library CDN: cache-first (laju & boleh offline)
  *  - Panggilan API Apps Script: SENTIASA rangkaian (jangan cache data pelajar)
  */
-const CACHE_NAME = 'sijilpro-v18';
+const CACHE_NAME = 'sijilpro-v19';
 const SHELL = ['./', './index.html', './manifest.json'];
 const CDN_HOSTS = ['cdnjs.cloudflare.com', 'docs.opencv.org', 'fonts.googleapis.com', 'fonts.gstatic.com'];
 
